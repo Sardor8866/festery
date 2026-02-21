4be1b0b00c79da7532a6aee575572ac12c891ca3
Sardor8866 <nictzmat@gmail.com>
2026-02-21 08:43:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -461,16 +461,16 @@ async def handle_text_message(message: Message, state: FSMContext):
 
     # ── КОМАНДА БАЛАНСА — отвечаем реплаем на сообщение пользователя ───
     if is_balance_command(message.text):
-        balance = sync_balances(message.from_user.id)
-        await message.reply(
-            f"<b><tg-emoji emoji-id=\"5278467510604160626\">💰</tg-emoji>: "
-            f"<code>{balance:,.2f}</code> "
-            f"<tg-emoji emoji-id=\"5197434882321567830\">💰</tg-emoji></b></blockquote>"
-            f"<blockquote><i>Выберите действие ниже<tg-emoji emoji-id=\"5201691993775818138\">💰</tg-emoji></i></blockquote>",
-            parse_mode=ParseMode.HTML,
-            reply_markup=get_balance_menu()
-        )
-        return
+    balance = sync_balances(message.from_user.id)
+    await message.reply(
+        f"<blockquote><b><tg-emoji emoji-id=\"5278467510604160626\">💰</tg-emoji> "
+        f"<code>{balance:,.2f}</code> "
+        f"<tg-emoji emoji-id=\"5197434882321567830\">💰</tg-emoji></b></blockquote>\n\n"
+        f"<blockquote><i>Выберите действие ниже <tg-emoji emoji-id=\"5201691993775818138\">💰</tg-emoji></i></blockquote>",
+        parse_mode=ParseMode.HTML,
+        reply_markup=get_balance_menu()
+    )
+    return
     # ────────────────────────────────────────────────────────────────────
 
     current_state = await state.get_state()
